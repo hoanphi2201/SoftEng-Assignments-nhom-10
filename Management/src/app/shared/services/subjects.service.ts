000387bc3ef316204f2dbee04effd5893ec0a3f4
anhducc13 <trantienduc10@gmai.com>
2019-04-05 20:47:06 +0700

    Đổ dữ liệu ra bảng, thông báo thành công - lỗi
    #48

--- Management/src/app/shared/services/subjects.service.ts
+++ Management/src/app/shared/services/subjects.service.ts
@@ -16,7 +16,14 @@ export class SubjectsService {
         private router: Router) {
 
     }
-
+    getItems(status: string, sort_field: string, sort_type: string, keyword: string ): Observable<ISubject[]> {
+        return this._httpService.get(`${this.apiUrl}/${status}/${sort_field}/${sort_type}?keyword=${keyword}`, {
+            withCredentials: true
+        }).pipe(
+            tap(_ => {}),
+            catchError(this.handleError<any>('getItems'))
+        );
+    }
     private handleError<T> (operation = 'operation', result?: T) {
         return (error: any): Observable<T> => {
             this.router.navigate(['/pages', 'errors']);
