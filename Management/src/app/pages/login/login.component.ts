f46658b9d3b9fe9976eb28cf4cafe4b74dcb1a7c
Phi Xuan Hoan <36753744+hoanphi2201@users.noreply.github.com>
2019-04-06 15:32:15 +0700

    Service lấy dữ liệu facebook

--- Management/src/app/pages/login/login.component.ts
+++ Management/src/app/pages/login/login.component.ts
@@ -14,14 +14,113 @@ declare const gapi: any;
 })
 
 export class LoginComponent implements OnInit, AfterViewInit {
-
+    test: Date = new Date();
+    private toggleButton: any;
+    private sidebarVisible: boolean;
+    private nativeElement: Node;
+    private formLogin: FormGroup;
+    returnUrl: string;
+    error: string = '';
+    public auth2: any;
     constructor(private element: ElementRef,
                 private _formBuilder: FormBuilder,
                 private route: ActivatedRoute,
                 private router: Router,
                 private authenticationService: AuthenticationService ) {
     }
-    ngOnInit(): void {
+    ngOnInit() {
+        // reset login status
+        // get return url from route parameters or default to '/'
+        this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+
+        let navbar : HTMLElement = this.element.nativeElement;
+        this.toggleButton = navbar.getElementsByClassName('navbar-toggle')[0];
+
+        setTimeout(function() {
+            // after 1000 ms we add the class animated to the login/register card
+            $('.card').removeClass('card-hidden');
+        }, 700);
+        // FORM
+        this.formLogin = this._formBuilder.group({
+            'username' : ['', [
+                Validators.required,
+                // Validators.pattern('[a-zA-Z0-9.-_]{1,}@[a-zA-Z.-]{2,}[.]{1}[a-zA-Z]{2,}')
+            ]],
+            'password' : ['', [
+                Validators.required
+            ]]
+        });
+
+
+
+        (window as any).fbAsyncInit = function() {
+            FB.init({
+                appId      : AppSettings.FACEBOOK_APP_ID,
+                cookie     : true,
+                xfbml      : true,
+                version    : 'v3.2'
+            });
+            FB.AppEvents.logPageView();
+        };
+
+        (function(d, s, id){
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) {return;}
+            js = d.createElement(s); js.id = id;
+            js.src = 'https://connect.facebook.net/en_US/sdk.js';
+            fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
+    }
+    sidebarToggle() {
+        var toggleButton = this.toggleButton;
+        var body = document.getElementsByTagName('body')[0];
+        var sidebar = document.getElementsByClassName('navbar-collapse')[0];
+        if (this.sidebarVisible == false) {
+            setTimeout(function() {
+                toggleButton.classList.add('toggled');
+            }, 500);
+            body.classList.add('nav-open');
+            this.sidebarVisible = true;
+        } else {
+            this.toggleButton.classList.remove('toggled');
+            this.sidebarVisible = false;
+            body.classList.remove('nav-open');
+        }
+    }
+    isFieldValid(form: FormGroup, field: string) {
+        return !form.get(field).valid && form.get(field).touched;
+    }
+    displayFieldCss(form: FormGroup, field: string) {
+        return {
+            'has-error': this.isFieldValid(form, field),
+            'has-feedback': this.isFieldValid(form, field)
+        };
+    }
+    // trường hợp cố ý bỏ disable
+    validateAllFormFields(formGroup: FormGroup) {
+        Object.keys(formGroup.controls).forEach(field => {
+            const control = formGroup.get(field);
+            if (control instanceof FormControl) {
+                control.markAsTouched({ onlySelf: true });
+            } else if (control instanceof FormGroup) {
+                this.validateAllFormFields(control);
+            }
+        });
+    }
+    onLoginWithFB(): void {
+        FB.login((response) => {
+            if (response.authResponse) {
+                FB.api('/me', {fields: 'name, email, first_name, last_name'}, (res) => {
+                        if (res && !res.error) {
+                            console.log(res);
+                            const data = Object.assign(res, response.authResponse);
+                        }
+                    }
+                );
+            } else {
+                showNotification('top', 'right', 300, `Rất tiếc, đã xảy ra lỗi !`);
+            }
+        });
     }
     ngAfterViewInit(): void {
     }
